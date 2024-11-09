3cbe20aa26ddf09e3b155a5bee2b637b9d0f9fdb
Mikasa-san <54586069+Mikasa-san@users.noreply.github.com>
2024-11-09 10:01:25 +0400

    Update

--- gui_app.py
+++ gui_app.py
@@ -317,17 +317,25 @@ class App(QtWidgets.QWidget):
         }
 
         report_crash_services = [
+                "com.apple.ReportCrash",
+                "com.apple.ReportCrash.Jetsam",
+                "com.apple.ReportMemoryException",
+                "com.apple.OTACrashCopier",
+                "com.apple.analyticsd",
                 "com.apple.aslmanager",
                 "com.apple.coresymbolicationd",
                 "com.apple.crash_mover",
                 "com.apple.crashreportcopymobile",
                 "com.apple.DumpBasebandCrash",
                 "com.apple.DumpPanic",
-                "com.apple.OTACrashCopier",
-                "com.apple.ReportCrash",
+                "com.apple.logd",
+                "com.apple.logd.admin",
+                "com.apple.logd.events",
+                "com.apple.logd.watchdog",
+                "com.apple.logd_reporter",
+                "com.apple.logd_reporter.report_statistics",
+                "com.apple.system.logger",
                 "com.apple.syslogd",
-                "com.apple.ReportMemoryException",
-                "com.apple.analyticsd",
         ]
 
         if self.disable_reportcrash_checkbox.isChecked():
