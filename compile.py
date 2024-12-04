8783911c1d0b414e5f09e59b38102e6bf5a05f7a
Mikasa-san <54586069+Mikasa-san@users.noreply.github.com>
2024-12-04 13:26:33 +0400

    Create compile.py

---
+++ compile.py
@@ -0,0 +1,26 @@
+from sys import platform
+
+import PyInstaller.__main__
+
+args = [
+    'gui_app.py',
+    '--hidden-import=pyimg4',
+    '--hidden-import=zeroconf',
+    '--hidden-import=zeroconf._utils.ipaddress',
+    '--hidden-import=zeroconf._handlers.answers',
+    '--copy-metadata=pyimg4',
+    '--onedir',
+    '--noconfirm',
+    '--name=QuietDaemon',
+    '--icon=icon.png',
+    '-y',
+    '--optimize=2'
+]
+
+PyInstaller.__main__.run(args)
+
+if platform == "darwin":
+    # add --windowed arg for macOS
+    args.append('--windowed')
+
+PyInstaller.__main__.run(args)
